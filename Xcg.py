ff05e4f063ac556df95f439052733aaf127e4972
GijsVugts <gijs.vugts.gv@gmail.com>
2018-03-15 11:37:25 +0100

    Update Xcg.py

--- Xcg.py
+++ Xcg.py
@@ -1,20 +1,29 @@
 import numpy as np
+#from Cit_par import *
 
 #datum line is nose of the aircraft
+def cgloc(state):
+    m_payload = 95+92+65+69+76+77+80+91+110+67
 
-m_payload = 95+92+65+69+76+77+80+91+110+67
+    if state == 1:      #normal state
+        Xcg_payload = 0.0254*(131*(95+92) + 214*(65+69) + 251*(76+77) + 288*(80+91) + 170*(110+67))/m_payload
+    if state == 2:      #moved cg
+        Xcg_payload = 0.0254*(131*(95+92) + 150*91 + 214*(65+69) + 251*(76+77) + 288*(80) + 170*(110+67))/m_payload
 
-Xcg_payload = 0.0254*(131*(95+92) + 214*(65+69) + 251*(76+77) + 288*(80+91) + 170*(110+67))/m_payload
-Xcg_payload2 = 0.0254*(131*(95+92) + 150*91 + 214*(65+69) + 251*(76+77) + 288*(80) + 170*(110+67))/m_payload
+    m_fuel = 2567*0.453592
 
-m_fuel = 2567*0.453592
+    Xcg_fuel = ((-713100+741533)*0.67+713100)/2567*0.0254
 
-Xcg_fuel = ((-713100+741533)*0.67+713100)/2567*0.0254
+    m_BEM = 9165*0.453592
 
-m_BEM = 9165*0.453592
+    Xcg_BEM = 292.18*0.0254
 
-Xcg_BEM = 292.18*0.0254
+    Xcg = (m_payload*Xcg_payload +m_fuel*Xcg_fuel + m_BEM*Xcg_BEM)/(m_payload+m_fuel+m_BEM)
+    return Xcg
 
-Xcg = (m_payload*Xcg_payload +m_fuel*Xcg_fuel + m_BEM*Xcg_BEM)/(m_payload+m_fuel+m_BEM)
 
-Xcg2 = (m_payload*Xcg_payload2 +m_fuel*Xcg_fuel + m_BEM*Xcg_BEM)/(m_payload+m_fuel+m_BEM)
+#---------------------------------------longitudinal stability ----------------------
+
+Xcg1 = cgloc(1)
+#Cmaa = CNwa * (Xcg - 261.56*0.0254-0.25*c)/c - CNha*(1-depsda)*Vh_V**2*Sh*lh/S/c
+
